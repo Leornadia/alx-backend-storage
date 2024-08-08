a758ed858e8f9cd92ac36e31fc196beeea194bc6
Leornadia <leornadiajose@gmail.com>
2024-08-08 14:46:36 -0700

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,5 @@
--- List all bands with "Glam rock" as their main style and rank them by their longevity
-SELECT 
-    band_name, 
-    CASE 
-        WHEN split IS NOT NULL THEN split - formed
-        ELSE 2022 - formed
-    END AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    main_style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
-
+-- List all glam rock bands ranked by longevity
+SELECT band_name, (2022 - formed) - split AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
