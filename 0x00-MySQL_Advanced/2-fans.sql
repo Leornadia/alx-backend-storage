3c9a24d4ad280fb73dcd59bd80e2a50fb11fe3db
Leornadia <leornadiajose@gmail.com>
2024-08-08 00:58:33 -0700

    2-fans.sql

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- 2-fans.sql
+-- Ranking country origins of bands by the number of (non-unique) fans
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
+
