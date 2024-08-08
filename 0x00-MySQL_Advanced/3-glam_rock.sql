8309515231d8a9e45f1e088b1288d7075247c000
Leornadia <leornadiajose@gmail.com>
2024-08-08 15:28:03 -0700

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,16 @@
-SELECT 
-    band_name, 
-    IFNULL(
-        CASE 
-            WHEN split IS NOT NULL THEN split - formed 
-            ELSE 2022 - formed 
-        END, 
-        0
-    ) AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC, band_name;
+-- 3-glam_rock.sql
+-- Lists all bands with 'Glam rock' as their main style, ranked by their longevity
 
+-- Import the metal_bands.sql.zip table dump
+-- CREATE DATABASE IF NOT EXISTS holberton;
+-- USE holberton;
+-- SOURCE metal_bands.sql;
+
+SELECT 
+    band_name,
+    IFNULL(2022 - formed, 0) AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
